a1fda4408513fb1a782d88ea66c30c5f656c2c77
Ali MOHAMED MOUSSA <alimohamed.moussa@gmail.com>
2020-02-09 19:22:50 +0100

    Ajout du cv en pdf

--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import { FaFilePdf } from 'react-icons/fa';
 import Social from '../components/Social';
 
 class Contact extends Component {
@@ -10,8 +12,12 @@ class Contact extends Component {
                 <h1 className="subtopic">Contact</h1>
                 <h3>Email  :   alimohamed.moussa@gmail.com</h3>
                 <h3>Tel   :   +33 (0)6 63 99 69 06</h3>
+                <div className="pdf-container">
+                    <h4>Télécharger mon cv en pdf</h4>
+                    <Link to="/public/cv/cv.pdf" target="_blank"  className="pdf-icon" download><FaFilePdf /></Link>
+                </div>
                 <Social />
-            </div>
+            </div>   
         )
     }
 }
