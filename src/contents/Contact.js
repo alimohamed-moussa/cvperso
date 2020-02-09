532035ab9a1ff94d22dd4227d4123265012430eb
Ali MOHAMED MOUSSA <alimohamed.moussa@gmail.com>
2020-02-09 15:47:19 +0100

    config responsive design

--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -9,7 +9,7 @@ class Contact extends Component {
             <div className="condiv">
                 <h1 className="subtopic">Contact</h1>
                 <h3>Email  :   alimohamed.moussa@gmail.com</h3>
-                <h3>Tel:   :   +33 (0)6 63 99 69 06</h3>
+                <h3>Tel   :   +33 (0)6 63 99 69 06</h3>
                 <Social />
             </div>
         )
